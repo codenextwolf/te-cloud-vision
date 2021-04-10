1a4254da27afc36c80d826e6c1b063ef0d4969bb
Denzale Reese <denzale@google.com>
2021-04-10 00:38:39 -0400

    add setup sh

---
+++ setup.sh
@@ -0,0 +1,1 @@
+pip3 install google-cloud-vision; pip3 install opencv-contrib-python==4.1.0.25; sudo apt-get install -y libatlas-base-dev libhdf5-dev libhdf5-serial-dev libjasper-dev  libqtgui4  libqt4-test
